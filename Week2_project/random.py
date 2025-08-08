9d0d5baa77a15d1ede32b9e50c8899afa6913fbd
emma9000 <loveemmaxu73@gmail.com>
2025-08-09 10:04:58 +1200

    add verification
    add a maximum number to limit customer inputting time

--- Week2_project/random.py
+++ Week2_project/random.py
@@ -9,21 +9,32 @@ def generate_word():
 
 def guess_letter(source_word,blanked_word):
     new_word=blanked_word
-    while True:
+    max_chance=len(source_word)+3
+    while max_chance > 0:
+        
         guess_letter=input("please input a letter:").lower()
         if len(guess_letter)!=1:
             print("Please input one letter")
             continue
-        
+        if(new_word.find(guess_letter)>=0):
+            print("You already tried that letter")
+            continue
+
         index_num=source_word.find(guess_letter)
         if index_num>=0:
             new_word=new_word[:index_num]+guess_letter+new_word[index_num+1:]
             print(new_word)
         else:
-            print("you were wrong") 
+            print(f"you were wrong") 
 
         if new_word==source_word:
+            print("Congratulation! You win!")
             break
+        
+        max_chance-=1
+        print(f"you have another {max_chance} chances")
+        if(max_chance==0):
+            print("You finished your chances")
 
     
 
