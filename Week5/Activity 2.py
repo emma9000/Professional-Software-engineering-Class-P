cbcc3024ff03ee845a43c23f8069a8d887ea1dec
emma9000 <loveemmaxu73@gmail.com>
2025-08-30 10:25:27 +1200

    fix: override method in the super parent

--- Week5/Activity 2.py
+++ Week5/Activity 2.py
@@ -7,12 +7,18 @@ class User:
         self.age=age
         self.ID=ID
 
+    def greet(self):
+        print(f" Greeting from the User class name is {self.name}")
+
 class Academics(User):
         
     def __init__(self,tax_code,salary,name,address,age,ID):
         super().__init__(name,address,age,ID)
         self.tax_code=tax_code
         self.salary=salary  
+    
+    def greet(self):
+        return super().greet()
 
 class Staff(User):
     
@@ -20,6 +26,9 @@ class Staff(User):
         super().__init__(name,address,age,ID)
         self.pay_rate=pay_rate
         self.office=office
+    def greet(self):
+        return super().greet()
+    
 
 class Student(Academics):
 
@@ -37,10 +46,10 @@ class Teacher(Staff):
         super().__init__(pay_rate,office,name,address,age,ID)
         self.title=title
         self.courseid=courseid
-    def great(self):
+    def greet(self):
         print(f"Hi, I'm your teacher {self.name}")
 
 student= Student("A123","B234","",0,"Emma","New Lynn",18,1)
-student.great()
+student.greet()
 teacher= Teacher("Professor",1,10,"lecture office","Emily","YB college location",30,1)
-teacher.great()
+teacher.greet()
