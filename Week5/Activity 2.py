13af33505d89175d60ced9b88eebb95e9771a92a
emma9000 <loveemmaxu73@gmail.com>
2025-08-30 09:42:57 +1200

    add: finish activity 1 and 2

---
+++ Week5/Activity 2.py
@@ -0,0 +1,36 @@
+
+class User:
+
+    def __init__(self,name,address,age,ID):
+        self.name=name
+        self.address
+        self.age
+        self.ID
+
+class Academics(User):
+        
+    def __init__(self,tax_code,salary,name,address,age,ID):
+        super().__init__(name,address,age,ID)
+        self.tax_code=tax_code
+        self.salary=salary  
+
+class Staff(User):
+    
+    def __init__(self,pay_rate,office,name,address,age,ID):
+        super().__init__(name,address,age,ID)
+        self.pay_rate=pay_rate
+        self.office=office
+
+class Student(Academics):
+
+    def __init__(self,acadamic_record,student_code,tax_code,salary,name,address,age,ID):
+        super().__init__(tax_code,salary,name,address,age,ID)
+        self.acadamic_record=acadamic_record
+        self.student_code=student_code
+    
+class Teacher(Staff)
+
+    def __init__(self,title,courseid,pay_rate,office,name,address,age,ID):
+        super().__init__(pay_rate,office,name,address,age,ID)
+        self.title=title
+        self.courseid=courseid
