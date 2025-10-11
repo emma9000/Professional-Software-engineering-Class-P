af912ff0f177365761f4f5e2fc88565b8e5f86a9
emma9000 <loveemmaxu73@gmail.com>
2025-10-12 11:24:30 +1300

    fix: update activaty2 with doctest

---
+++ week11/activaty3.py
@@ -0,0 +1,7 @@
+
+# from cost_manager import add_cost, calculate_total_cost
+import cost_manager
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(cost_manager, verbose=True)
--- week11/cost_manager.py
+++ week11/cost_manager.py
@@ -1,11 +1,11 @@
-from db import create_connection
+from db import create_connection,create_table
 
 def add_cost(amount, description):
     conn = create_connection()
     cursor = conn.cursor()
     cursor.execute("INSERT INTO Cost (amount, description) VALUES (?, ?)", (amount, description))
     conn.commit()
-    print(" cost added successfully.")
+    # print(" cost added successfully.")
     conn.close()
 
 def view_costs():
@@ -16,7 +16,26 @@ def view_costs():
     conn.close()
     return rows
 
+
+def clear_costs():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Cost")
+    conn.commit()
+    conn.close()
+
 def calculate_total_cost():
+    """
+    Return the total cost from the Cost table.
+
+    example: 
+    >>> create_table()
+    >>> clear_costs()
+    >>> add_cost(100.0, "Groceries")
+    >>> add_cost(50.0, "Transport")
+    >>> calculate_total_cost()
+    150.0
+    """
     conn = create_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT SUM(amount) FROM Cost")
