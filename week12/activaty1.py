9ae00c0783aab8c37264a315a8e9c97522804457
emma9000 <loveemmaxu73@gmail.com>
2025-10-18 08:56:33 +1300

    fix: change the command of activity 2

--- week12/activaty1.py
+++ week12/activaty1.py
@@ -12,7 +12,7 @@ def learn_flask(name):
 #if __name__ == "__main__":
 #    app.run(debug=True)
 
-# activity 1.3
+# activity 1.2
 # Add a new route that takes an integer parameter and returns its square
 # user should input an integer in the URL like /cal/4 and the output should be "The square of 4 is 16"
 @app.route('/cal/<int:number>')
