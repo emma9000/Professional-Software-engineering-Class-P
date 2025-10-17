2e3e877a4793fcad4bb62d10b8f46aebe2765bae
emma9000 <loveemmaxu73@gmail.com>
2025-10-18 08:55:49 +1300

    feat: add activaty 1.3

--- week12/activaty1.py
+++ week12/activaty1.py
@@ -11,3 +11,11 @@ def learn_flask(name):
     return f"{name} is learning Flask!"
 #if __name__ == "__main__":
 #    app.run(debug=True)
+
+# activity 1.3
+# Add a new route that takes an integer parameter and returns its square
+# user should input an integer in the URL like /cal/4 and the output should be "The square of 4 is 16"
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"The square of {number} is {number**2}"
+ 
